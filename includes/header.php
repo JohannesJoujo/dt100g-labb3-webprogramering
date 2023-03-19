339faba04ed483f04fc337422ebbe063a5258844
JohannesJoujo <jagj2723@gmail.com>
2023-03-19 13:09:21 +0100

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<?php
+
+if($_SESSION["valid_user"] != session_id()){
+    header("Location: index.php");
+    exit;
+}
+if(isset($_REQUEST["logout"])){
+    unset($_SESSION["valid_user"]);
+    session_destroy();
+    header("Location: index.php");
+    exit;
+}
+
+include("/Applications/XAMPP/xamppfiles/htdocs/phpm/includes/mainmenu.php");
